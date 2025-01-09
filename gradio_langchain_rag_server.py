1f5f8872837ed033bf6604bad71536456aa0e77d
ElectriPixie <ElectriPixie@gmail.com>
2025-01-09 17:54:59 -0500

    changed SentenceTransformerEmbeddings

--- gradio_langchain_rag_server.py
+++ gradio_langchain_rag_server.py
@@ -20,8 +20,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""
 class SentenceTransformerEmbeddings(Embeddings):
     def __init__(self, model_name: str, pt_model: str = None):
         self.model = SentenceTransformer(model_name)
-        if pt_model:
-            self.model.load_state_dict(torch.load(pt_model, map_location=torch.device('cpu'), weights_only=True))
+        self.model.load_state_dict(torch.load(pt_model, map_location=torch.device('cpu'), weights_only=True))
 
     def embed_query(self, query: str):
         return self.model.encode([query], convert_to_tensor=True)[0].cpu().numpy()
