7b9782be0a7aa3319dfddffc7d3f2d2f28552ddf
ElectriPixie <ElectriPixie@gmail.com>
2025-01-14 16:26:42 -0500

    added a config folder and seperated some shared functions into a file

--- pylib/initializeFaissStore.py
+++ pylib/initializeFaissStore.py
@@ -8,46 +8,15 @@ from langchain_community.vectorstores import FAISS
 from langchain.embeddings.base import Embeddings
 import argparse
 import psutil
-
-def add_trailing_slash(path):
-    if not path.endswith('/'):
-        path += '/'
-    return path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'sharedFunctions'))
+from sharedFunctions import add_trailing_slash, get_program_name
+sys.path.append(os.path.join(os.path.dirname(__file__), 'config'))
+from config import DEFAULT_MODEL_DIR, DEFAULT_MODEL_NAME, DEFAULT_VSTORE_DIR, DEFAULT_VSTORE_NAME
 
 SCRIPT_DIR = os.path.dirname(__file__)
 DEFAULT_PATH = add_trailing_slash(os.path.dirname(SCRIPT_DIR))
-DEFAULT_VSTORE_NAME="Book_Collection"
-DEFAULT_VSTORE_DIR="faiss_store"
-DEFAULT_MODEL_DIR="models"
-DEFAULT_MODEL_NAME="all-MiniLM-L6-v2"
 
-# Get the parent process ID
-parent_pid = os.getppid()
-
-# Fetch the parent process
-parent_process = psutil.Process(parent_pid)
-
-# Use cmdline to get the full command with arguments
-parent_cmdline = parent_process.cmdline()
-
-# Extract the calling script name from cmdline
-if len(parent_cmdline) > 1:  # Check if there are arguments
-    run_script_name = os.path.basename(parent_cmdline[1])  # Get only the file name
-else:
-    run_script_name = "Unknown"
-
-# Get the current Python script name
-script_name = os.path.basename(__file__)
-
-# Remove extensions for comparison
-run_script_base = os.path.splitext(run_script_name)[0]
-script_base = os.path.splitext(script_name)[0]
-
-# Compare base names
-if run_script_base == script_base:
-    prog_name=run_script_name
-else:
-    prog_name=script_name
+prog_name = get_program_name()
 
 parser = argparse.ArgumentParser()
 # Define the name of the vector store
